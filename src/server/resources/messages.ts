546389cded10337ce4c4a2b6981e61f017962db8
Tim Culleton <mit_notelluc@hotmail.com>
2019-09-30 14:53:20 +1000

    Add basic messages/replace

---
+++ src/server/resources/messages.ts
@@ -0,0 +1,32 @@
+// tslint:disable: max-line-length
+
+export enum ProcessErrorMessage {
+    CAN_NOT_CREATE_NEW_PROCESS_ORIGINAL_STILL_RUNNING =
+        "Can not create new process {command} as original process {originalCommand} still running.",
+    CAN_NOT_WRITE_TO_PROCESS_DOES_NOT_EXIST =
+        "Can not write to process as it has not been created",
+}
+
+export interface IProcessCanNotCreateNewProcessOriginalStillRunningData {
+    command: string;
+    originalCommand: string;
+}
+
+export type ValueType<T> =
+    T extends ProcessErrorMessage.CAN_NOT_CREATE_NEW_PROCESS_ORIGINAL_STILL_RUNNING ? IProcessCanNotCreateNewProcessOriginalStillRunningData :
+    T extends string ? "object" : "undefined";
+
+/**
+ *
+ * For the supplied message replace any values in the '{}' with values from the supplied values object.
+ * @export
+ * @template T extends string: typically this will be a defined message which will set the valueType
+ * @param {T} message - Message that has properties to be replaced
+ * @param {ValueType<T>} values - Object which contains values that will be inserted into the messag
+ * @returns {string}
+ */
+export function replace<T extends string>(message: T, values: ValueType<T>): string {
+    return message.replace(/\{([\w\-]+)\}/g, (m, name) => {
+        return values[name] !== undefined ? values[name] : m;
+    });
+}
